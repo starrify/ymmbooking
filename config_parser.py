92810fa0cef72241404266aa16b035c36b804719
Pengyu CHEN <cpy.prefers.you@gmail.com>
2013-06-02 00:46:11 +0800

    ADDED: initial commit ADDED: a quite simple framework of bottle

---
+++ config_parser.py
@@ -0,0 +1,15 @@
+# This is part of coursework of Software Engineering, 2013.02-2013.07
+# Source code by Pengyu CHEN (cpy.prefers.you[at]gmail.com)
+# COPYLEFT, ALL WRONGS RESERVED
+
+class _config():
+    def __init__(self, config_path='./config.cfg'): 
+        import configparser
+        parser = configparser.ConfigParser()
+        parser.read(config_path)
+
+        # the following parsings shall not fail
+        self.database_path = parser.get('Path', 'database_path')
+        self.static_path = parser.get('Path', 'static_path')
+
+config = _config()
---
+++ ymm.py
@@ -0,0 +1,19 @@
+# This is part of coursework of Software Engineering, 2013.02-2013.07
+# Source code by Pengyu CHEN (cpy.prefers.you[at]gmail.com)
+# COPYLEFT, ALL WRONGS RESERVED
+
+import bottle
+from config_parser import config
+
+app = bottle.Bottle()
+
+@app.route('/')
+def index():
+    return "well here i'm just a testing message"
+
+@app.route('/static/<filepath:path>')
+def static(filepath):   
+    return bottle.static_file(filepath, root=config.static_path)
+
+bottle.run(app, host='localhost', port=8080, debug=True)
+
