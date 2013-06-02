b0c506db219d59afbdfd2ecfb9c0b8c21b1864ed
Pengyu CHEN <cpy.prefers.you@gmail.com>
2013-06-03 03:51:32 +0800

    CHANGED: routing structure ADDED: routes of GET method with flight searches ADDED: oneway.tpl ADDED: prototype of auth_validate

---
+++ template/flight/oneway.tpl
@@ -0,0 +1,7 @@
+Oneway flight~<br>
+%if flights:
+%   for item in flights:
+<strong>{{item[4]}}</strong> : {{item}}<br>
+%   end
+%end
+end of oneway flight~<br>
--- ymm.py
+++ ymm.py
@@ -5,18 +5,60 @@
 import bottle
 from config_parser import config
 
-app = bottle.Bottle()
+class App():
+    def __init__(self):
+        #self.app = bottle.Bottle()
+        pass
+    app = bottle.Bottle()
 
-@app.route('/')
-@app.route('/index')
-@bottle.view(config.template_path + 'index.tpl')
-def index():
-    return {}
+    def auth_validate(func):
+        def decorator(*args, **kwargs):
+            # validation here
+            return func(*args, **kwargs)
+        return decorator
 
-# routes static files
-@app.route('/static/<filepath:path>')
-def view(filepath):   
-    return bottle.static_file(filepath, root=config.static_path)
+    # routes static files
+    @app.route('/static/<filepath:path>')
+    def static(filepath):   
+        return bottle.static_file(filepath, root=config.static_path)
 
-bottle.run(app, host='localhost', port=8080, debug=True)
+    @app.route('/')
+    @app.route('/index')
+    @bottle.view(config.template_path + 'index.tpl')
+    def index():
+        return {}
+
+    @app.get('/flight/oneway')
+    @bottle.view(config.template_path + 'flight/oneway.tpl')
+    @auth_validate
+    def oneway_get():
+        return {'flights': [
+            ['Airport 1', 'Airport 2', 'Datetime 1', 'Datetime 2', 'FlighNo'], 
+            ['Airport 1', 'Airport 2', 'Datetime 1', 'Datetime 2', 'FlighNo'], 
+        ]}
+
+    @app.get('/flight/oneway_inter')
+    @bottle.view(config.template_path + 'flight/oneway_inter.tpl')
+    def oneway_inter_get():
+        return {}
+
+    @app.get('/flight/roundtrip')
+    @bottle.view(config.template_path + 'flight/roundtrip.tpl')
+    def roundtrip_get():
+        return {}
+
+    @app.get('/flight/roundtrip_inter')
+    @bottle.view(config.template_path + 'flight/roundtrip_inter.tpl')
+    def roundtrip_inter_get():
+        return {}
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    application = App()
+    bottle.run(application.app, host='localhost', port=8080, debug=True)
 
